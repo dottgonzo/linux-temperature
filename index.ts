ebdeb279e603a1787b4f0deb5ea37059ea6b3d8d
dario <darioyzf@gmail.com>
2016-11-11 12:46:00 +0100

    up

--- index.ts
+++ index.ts
@@ -6,12 +6,30 @@ import * as child_process from 'child_process'
 const pathExists = require('path-exists');
 
 
+interface Icores {
+
+temp:number;
+unit:string
+corenumber:number
+
+}
+
+
+interface Itemp {
+
+media:number
+unit:string
+
+cores: Icores[]
+
+}
+
 
 const exec = child_process.exec
 
 
 function getTemp(tocat: string, unit?: string) {
-    return new Promise<number>((resolve, reject) => {
+    return new Promise<Itemp>((resolve, reject) => {
 
         exec('cat ' + tocat, (error, stdout, stderr) => {
 
@@ -46,8 +64,10 @@ function getTemp(tocat: string, unit?: string) {
 export default function temp(unit?: string) {
     return new Promise((resolve, reject) => {
 
-        const tocat0 = '/sys/class/thermal/thermal_zone0/temp'
-        const tocat1 = '/sys/class/thermal/thermal_zone0/temp'
+// per i sistemi multicore vanno controllati + file e va fatta una media (se siamo ppignoli)
+
+        const tocat0 = '/sys/class/thermal/thermal_zone0/temp' // controlla se da qualche parte ci sono minimi e massimi
+        const tocat1 = '/sys/devices/platform/coretemp.0/hwmon/hwmon1/temp2_input' // qui i minimi e massimi ci sono sicuro
 
         pathExists(tocat0).then(exists => {
             if (exists) {
